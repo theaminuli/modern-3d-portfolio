647c115724f39bc59176cb7473c8547e2082962f
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-08 10:47:19 +0600

    update Hero component styles and add violet gradient utility class

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
 
 			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
 				<a href="#about">
-					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-[#aaa6c3] flex justify-center items-start p-2">
 						<motion.div
 							animate={{
 								y: [0, 24, 0],
@@ -50,7 +50,7 @@ const Hero = () => {
 								repeat: Infinity,
 								repeatType: "loop",
 							}}
-							className="w-3 h-3 rounded-full bg-secondary mb-1"
+							className="w-3 h-3 rounded-full bg-[#aaa6c3] mb-1"
 						/>
 					</div>
 				</a>
