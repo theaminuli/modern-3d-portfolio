df170899cf01aea2797578611dbff972c743e6b3
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-08 10:51:44 +0600

    update Hero component to use a new violet background color and enhance the violet gradient styling

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 				className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5`}
 			>
 				<div className="flex flex-col justify-center items-center mt-5">
-					<div className="w-5 h-5 rounded-full bg-[#323133]" />
+					<div className="w-5 h-5 rounded-full bg-[#804dee]" />
 					<div className="w-1 sm:h-80 h-40 violet-gradient" />
 				</div>
 
