1721d6a2b85d29786cb4f56615d6efc37e39cb75
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-09 23:20:19 +0600

    refactor Computers component; enhance lighting setup with ambient, hemisphere, and point lights for improved visibility and dramatic effect

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,67 +1,50 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import { Vector3 } from "three";
-import { useRef } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useGLTF, SpotLight, useDepthBuffer } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 
-const Computers = () => {
-  return (
-    <Canvas
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [-2, 2, 6], fov: 50, near: 1, far: 20 }}
-    >
-      <color attach="background" args={["#1a1a1a"]} />
-      <fog attach="fog" args={["#1a1a1a", 5, 20]} />
-      <ambientLight intensity={0.3} />
-      <Scene />
-    </Canvas>
-  );
+/**
+ * Computers component renders a 3D computer model with enhanced lighting.
+ *
+ * @param {Object} props - The component props.
+ * @param {boolean} props.isMobile - Determines if the view is on a mobile device.
+ *
+ * @returns {JSX.Element} The rendered 3D computer model with better lighting.
+ */
+const Computers = ({ isMobile }) => {
+	const computer = useGLTF("./desktop_pc/scene.gltf");
+
+	return (
+		<mesh>
+			{/* Soft ambient lighting for better visibility */}
+			<ambientLight intensity={0.9} />
+
+			{/* Hemisphere light for a more natural feel */}
+			<hemisphereLight intensity={0.8} groundColor="black" />
+
+			{/* Stronger spotlight for dramatic effect */}
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={0.3}
+				penumbra={0.8}
+				intensity={2.5}
+				castShadow
+				shadow-mapSize={2048}
+			/>
+
+			{/* Additional point lights for general brightness */}
+			<pointLight position={[5, 10, 5]} intensity={1.8} />
+			<pointLight position={[-5, 10, -5]} intensity={1.5} />
+			<pointLight position={[0, 5, 0]} intensity={1.2} />
+
+			{/* Computer model */}
+			<primitive
+				object={computer.scene}
+				scale={isMobile ? 0.7 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
+			/>
+		</mesh>
+	);
 };
-
-function Scene() {
-  const depthBuffer = useDepthBuffer({ frames: 1 });
-  const computer = useGLTF("./desktop_pc/scene.gltf");
-
-  return (
-    <>
-      {/* Moving Spotlights for dynamic lighting */}
-      <MovingSpot depthBuffer={depthBuffer} color="#00aaff" position={[3, 3, 2]} />
-      <MovingSpot depthBuffer={depthBuffer} color="#ff5500" position={[-3, 3, 2]} />
-
-      {/* 3D Computer Model */}
-      <mesh>
-        <primitive
-          object={computer.scene}
-          scale={0.75}
-          position={[0, -3, -1.5]}
-          rotation={[-0.01, -0.2, -0.1]}
-        />
-      </mesh>
-
-      {/* Floor Plane for shadows */}
-      <mesh receiveShadow position={[0, -3, 0]} rotation-x={-Math.PI / 2}>
-        <planeGeometry args={[50, 50]} />
-        <meshStandardMaterial color="#222" />
-      </mesh>
-    </>
-  );
-}
-
-function MovingSpot({ vec = new Vector3(), ...props }) {
-  const light = useRef();
-  const viewport = useThree((state) => state.viewport);
-
-  useFrame((state) => {
-    light.current.target.position.lerp(
-      vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0),
-      0.1
-    );
-    light.current.target.updateMatrixWorld();
-  });
-
-  return <SpotLight castShadow ref={light} penumbra={1} distance={10} angle={0.4} intensity={4} attenuation={5} anglePower={4} {...props} />;
-}
 
 export default Computers;
