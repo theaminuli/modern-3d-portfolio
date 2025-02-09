62d7b007e7ab03df01833b5cebf806164887b284
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-09 22:54:03 +0600

    enhance lighting and shadow effects in Computers component; adjust scale and position for improved rendering

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -15,20 +15,20 @@ const Computers = ({ isMobile }) => {
 
 	return (
 		<mesh>
-			<hemisphereLight intensity={0.15} groundColor="black" />
+			<hemisphereLight intensity={0.2} groundColor="black" />
 			<spotLight
 				position={[-20, 50, 10]}
-				angle={0.12}
-				penumbra={1}
-				intensity={1}
+				angle={0.3} // Wider beam angle
+				penumbra={0.8} // Slightly softer edges
+				intensity={2.5} // Increased brightness
 				castShadow
-				shadow-mapSize={1024}
+				shadow-mapSize={2048} // Higher resolution shadows
 			/>
-			<pointLight intensity={1} />
+			<pointLight intensity={1.5} />
 			<primitive
 				object={computer.scene}
-				scale={isMobile ? 0.7 : 0.9}
-				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -2.5]}
+				scale={isMobile ? 0.7 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
 				rotation={[-0.01, -0.2, -0.1]}
 			/>
 		</mesh>
