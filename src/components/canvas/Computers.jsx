5984771688c118576895c26dda8e8801c59de560
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-09 23:18:13 +0600

    refactor Computers component; enhance ambient light intensity and adjust spotlight properties for improved dynamic lighting

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,16 +5,20 @@ import { useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useGLTF, SpotLight, useDepthBuffer } from "@react-three/drei";
 
-export default function App() {
+const Computers = () => {
   return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [-2, 2, 6], fov: 50, near: 1, far: 20 }}>
+    <Canvas
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [-2, 2, 6], fov: 50, near: 1, far: 20 }}
+    >
       <color attach="background" args={["#1a1a1a"]} />
       <fog attach="fog" args={["#1a1a1a", 5, 20]} />
-      <ambientLight intensity={0.2} />
+      <ambientLight intensity={0.3} />
       <Scene />
     </Canvas>
   );
-}
+};
 
 function Scene() {
   const depthBuffer = useDepthBuffer({ frames: 1 });
@@ -22,13 +26,18 @@ function Scene() {
 
   return (
     <>
-      {/* Moving Spotlights for better lighting */}
+      {/* Moving Spotlights for dynamic lighting */}
       <MovingSpot depthBuffer={depthBuffer} color="#00aaff" position={[3, 3, 2]} />
       <MovingSpot depthBuffer={depthBuffer} color="#ff5500" position={[-3, 3, 2]} />
 
       {/* 3D Computer Model */}
       <mesh>
-        <primitive object={computer.scene} scale={0.75} position={[0, -3, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
+        <primitive
+          object={computer.scene}
+          scale={0.75}
+          position={[0, -3, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
       </mesh>
 
       {/* Floor Plane for shadows */}
@@ -52,5 +61,7 @@ function MovingSpot({ vec = new Vector3(), ...props }) {
     light.current.target.updateMatrixWorld();
   });
 
-  return <SpotLight castShadow ref={light} penumbra={1} distance={10} angle={0.4} intensity={3} attenuation={5} anglePower={4} {...props} />;
+  return <SpotLight castShadow ref={light} penumbra={1} distance={10} angle={0.4} intensity={4} attenuation={5} anglePower={4} {...props} />;
 }
+
+export default Computers;
