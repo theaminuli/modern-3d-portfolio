f089930ea34058488bf988c3f10234bb8e2b4e04
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-09 23:03:36 +0600

    enhance dynamic lighting in Computers component; implement interactive spotlight effects and adjust light positioning for improved visual rendering

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+import { Vector3 } from "three";
+import { useGLTF, SpotLight, useDepthBuffer } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 
 /**
- * Computers component renders a 3D computer model with lighting effects.
+ * Computers component renders a 3D computer model with dynamic lighting effects.
  *
  * @param {Object} props - The component props.
  * @param {boolean} props.isMobile - Determines if the view is on a mobile device.
@@ -12,19 +15,13 @@ import { useGLTF } from "@react-three/drei";
  */
 const Computers = ({ isMobile }) => {
 	const computer = useGLTF("./desktop_pc/scene.gltf");
+	const depthBuffer = useDepthBuffer({ frames: 1 });
 
 	return (
 		<mesh>
 			<hemisphereLight intensity={0.3} groundColor="black" />
-			<spotLight
-				position={[-10, 30, 5]} // Lowered the spotlight closer to the object
-				angle={0.5} // Wider beam
-				penumbra={0.6} // Slightly soft edges
-				intensity={5} // Increased brightness significantly
-				castShadow
-				shadow-mapSize={4096} // Higher resolution shadows for sharpness
-			/>
-			<pointLight intensity={2} position={[0, 5, 0]} /> {/* Stronger point light from above */}
+			<DynamicSpot depthBuffer={depthBuffer} color="#ffcc00" position={[-5, 10, 5]} />
+			<DynamicSpot depthBuffer={depthBuffer} color="#0077ff" position={[5, 10, -5]} />
 			<primitive
 				object={computer.scene}
 				scale={isMobile ? 0.7 : 0.75}
@@ -34,5 +31,32 @@ const Computers = ({ isMobile }) => {
 		</mesh>
 	);
 };
+
+function DynamicSpot({ vec = new Vector3(), ...props }) {
+	const light = useRef();
+	const viewport = useThree((state) => state.viewport);
+
+	useFrame((state) => {
+		light.current.target.position.lerp(
+			vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0),
+			0.1
+		);
+		light.current.target.updateMatrixWorld();
+	});
+
+	return (
+		<SpotLight
+			castShadow
+			ref={light}
+			penumbra={0.8}
+			distance={10}
+			angle={0.4}
+			attenuation={6}
+			anglePower={5}
+			intensity={4}
+			{...props}
+		/>
+	);
+}
 
 export default Computers;
