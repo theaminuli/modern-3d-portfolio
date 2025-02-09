bdd2363a4738f0b4e2c5bce21c5067b919e9882e
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-09 23:07:48 +0600

    refactor Computers component; simplify lighting setup by removing dynamic spotlight and adjusting hemisphereLight and spotLight properties for improved performance

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,12 +1,9 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import { useRef } from "react";
-import { Vector3 } from "three";
-import { useGLTF, SpotLight, useDepthBuffer } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 
 /**
- * Computers component renders a 3D computer model with dynamic lighting effects.
+ * Computers component renders a 3D computer model with lighting effects.
  *
  * @param {Object} props - The component props.
  * @param {boolean} props.isMobile - Determines if the view is on a mobile device.
@@ -15,13 +12,19 @@ import { useFrame, useThree } from "@react-three/fiber";
  */
 const Computers = ({ isMobile }) => {
 	const computer = useGLTF("./desktop_pc/scene.gltf");
-	const depthBuffer = useDepthBuffer({ frames: 1 });
 
 	return (
 		<mesh>
-			<hemisphereLight intensity={0.3} groundColor="black" />
-			<DynamicSpot depthBuffer={depthBuffer} color="#ffcc00" position={[-5, 10, 5]} />
-			<DynamicSpot depthBuffer={depthBuffer} color="#0077ff" position={[5, 10, -5]} />
+			<hemisphereLight intensity={0.15} groundColor="black" />
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={0.12}
+				penumbra={1}
+				intensity={1}
+				castShadow
+				shadow-mapSize={1024}
+			/>
+			<pointLight intensity={1} />
 			<primitive
 				object={computer.scene}
 				scale={isMobile ? 0.7 : 0.75}
@@ -31,32 +34,5 @@ const Computers = ({ isMobile }) => {
 		</mesh>
 	);
 };
-
-function DynamicSpot({ vec = new Vector3(), ...props }) {
-	const light = useRef();
-	const viewport = useThree((state) => state.viewport);
-
-	useFrame((state) => {
-		light.current.target.position.lerp(
-			vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.height) / 2, 0),
-			0.1
-		);
-		light.current.target.updateMatrixWorld();
-	});
-
-	return (
-		<SpotLight
-			castShadow
-			ref={light}
-			penumbra={0.8}
-			distance={10}
-			angle={0.4}
-			attenuation={6}
-			anglePower={5}
-			intensity={4}
-			{...props}
-		/>
-	);
-}
 
 export default Computers;
