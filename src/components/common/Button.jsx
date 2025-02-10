ea5cea9f06510c8f0cca027d79034b8545441099
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-10 23:09:04 +0600

    refactor Button component; adjust ping animation opacity and improve styling for copy container in CSS

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable react/prop-types */
 const Button = ({ name, isBeam = false, containerClass }) => {
-  return (
-    <button className={`btn ${containerClass}`}>
-      {isBeam && (
-        <span className="relative flex h-3 w-3">
-          <span className="rounded-full bg-green-500 opacity-75 animate-ping"></span>
-          <span className="relative inline-flex w-[.75rem] h-[.75rem] rounded-full bg-green-500"></span>
-        </span>
-      )}
-      {name}
-    </button>
-  );
+	return (
+		<button className={`btn ${containerClass}`}>
+			{isBeam && (
+				<span className="relative flex h-3 w-3">
+					<span className="rounded-full bg-green-500 opacity-25 animate-ping"></span>
+					<span className="relative inline-flex w-[.75rem] h-[.75rem] rounded-full bg-green-500"></span>
+				</span>
+			)}
+			{name}
+		</button>
+	);
 };
 
 export default Button;
