c12f61f31cdd2f4d2b3ef5564f3ed1806664125d
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-10 23:22:23 +0600

    refactor Button component; replace ping animation with CSS class and define keyframes in CSS

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ name, isBeam = false, containerClass }) => {
 		<button className={`btn ${containerClass}`}>
 			{isBeam && (
 				<span className="relative flex h-3 w-3">
-					<span className="rounded-full bg-green-500 opacity-25 animate-ping"></span>
+					<span className="btn-ping"></span>
 					<span className="relative inline-flex w-[.75rem] h-[.75rem] rounded-full bg-green-500"></span>
 				</span>
 			)}
