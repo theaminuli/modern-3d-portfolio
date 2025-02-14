b28e55f5d9fb8e5dd54eb057f9f50fc9b993c876
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-14 22:33:10 +0600

    refactor: remove unused BallCanvas and EarthCanvas exports from index.jsx

--- src/components/canvas/index.jsx
+++ src/components/canvas/index.jsx
@@ -1,7 +1,5 @@
-import BallCanvas from "./BallCanvas";
 import ComputersCanvas from "./ComputersCanvas";
 import Developer from "./Developer";
-import EarthCanvas from "./EarthCanvas";
 import StarsCanvas from "./StarsCanvas";
 
-export { BallCanvas, ComputersCanvas, Developer, EarthCanvas, StarsCanvas };
+export { ComputersCanvas, Developer, StarsCanvas };
