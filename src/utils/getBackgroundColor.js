d02610efc021df3ad29c36cb1442e67171005b8d
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-02-15 12:21:30 +0600

    refactor: update predefined colors in getBackgroundColor utility

--- src/utils/getBackgroundColor.js
+++ src/utils/getBackgroundColor.js
@@ -1,4 +1,4 @@
-const predefinedColors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#F4D03F"];
+const predefinedColors = ["#008236", "#625FFF", "#3357FF", "#FF33A1", "#F4D03F"];
 
 function getBackgroundColor(index) {
 	return predefinedColors[index % predefinedColors.length]; // Ensures looping
